9c9d73b250e6adf6622909514c86a7ce1039dbba
philhannant <philip.a.hannant@gmail.com>
2018-05-31 21:09:13 +0100

    added distance to range

--- src/main/scala/optimisers/Optimiser.scala
+++ src/main/scala/optimisers/Optimiser.scala
@@ -1,12 +1,26 @@
 package optimisers
-import Model.{Point, Result, Scenario}
+import Model._
 
 class Optimiser extends PowerOptimiser {
 
   override def optimise(scenario: Scenario): Result = ???
 
-  def calculateChebysev(p1: Point, p2: Point): Int = {
+  def calculateChebyshev(p1: Point, p2: Point): Int = {
     Math.max(Math.abs(p1.x - p2.x), Math.abs(p1.y - p2.y))
   }
 
+  def outOfRangeCheck(t: Transmitter, r: Receiver): Boolean = {
+    calculateChebyshev(t.location, r.location) - t.power > 0
+  }
+
+  def distanceToRange(t: Transmitter, r: Receiver): Int = {
+    Math.abs(calculateChebyshev(t.location, r.location) - t.power)
+  }
+
+  def getOutOfRangeReceivers(transmitters: List[Transmitter], receivers: List[Receiver]): List[Receiver] = {
+    match receivers{
+      case x :: xs => ???
+      case Nil => receivers
+    }
+  }
 }
