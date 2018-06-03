1a93b97f17c144d87c9b30af2bf766b1d33a4f86
philhannant <philip.a.hannant@gmail.com>
2018-06-03 22:09:28 +0100

    tests corrected

--- src/test/scala/PowerOptimiserTest.scala
+++ src/test/scala/PowerOptimiserTest.scala
@@ -9,31 +9,31 @@ class PowerOptimiserTest extends FlatSpec with Matchers {
   val powerOptimiser: Optimiser = new Optimiser
   val testScenarios: TestScenarios = new TestScenarios{}
 
-  "optimise" should "return Result containing a list of Transmitters" in {
+  "optimise" should "return Result containing a list of Transmitters0" in {
     powerOptimiser.optimise(testScenarios.testScenarios(0).scenario) should be (testScenarios.testScenarios(0).expectedResult)
   }
 1
-  "optimise" should "return Result containing a list of Transmitters" in {
+  "optimise" should "return Result containing a list of Transmitters1" in {
     powerOptimiser.optimise(testScenarios.testScenarios(1).scenario) should be (testScenarios.testScenarios(1).expectedResult)
   }
 
-  "optimise" should "return Result containing a list of Transmitters" in {
+  "optimise" should "return Result containing a list of Transmitters2" in {
     powerOptimiser.optimise(testScenarios.testScenarios(2).scenario) should be (testScenarios.testScenarios(2).expectedResult)
   }
 
-  "optimise" should "return Result containing a list of Transmitters" in {
+  "optimise" should "return Result containing a list of Transmitters3" in {
     powerOptimiser.optimise(testScenarios.testScenarios(3).scenario) should be (testScenarios.testScenarios(3).expectedResult)
   }
 
-  "optimse" should "return Result containing a list of Transmitters" in {
+  "optimse" should "return Result containing a list of Transmitters4" in {
     powerOptimiser.optimise(testScenarios.testScenarios(4).scenario) should be (testScenarios.testScenarios(4).expectedResult)
   }
 
-  "optimise" should "return Result containing a list of Transmitters" in {
+  "optimise" should "return Result containing a list of Transmitters5" in {
     powerOptimiser.optimise(testScenarios.testScenarios(5).scenario) should be (testScenarios.testScenarios(5).expectedResult)
   }
 
-  "optimise" should "return Result containing a list of Transmitters" in {
+  "optimise" should "return Result containing a list of Transmitters6" in {
     powerOptimiser.optimise(testScenarios.testScenarios(6).scenario) should be (testScenarios.testScenarios(6).expectedResult)
   }
 }
